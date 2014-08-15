971a12cd412fee29d40bb52be1717955fc4bb232
Benjamin Hutchins <ben@hutchins.co>
2014-08-15 14:48:39 -0400

    Decupling to make support for more than just webpagetest service.

--- tasks/web-performance.js
+++ tasks/web-performance.js
@@ -5,109 +5,119 @@ var path = require('path');
 var _ = require('lodash');
 
 module.exports = function (grunt) {
-    var TestModel = require('../lib/testmodel')(grunt);
-
-    var testDefaults = {
-        key: null, // api key, optional
-        url: null, // required, url of page to test, must be publicly accessible
-        label: null, // label for the test
-        location: 'Dulles_IE9.DSL', // look at http://www.webpagetest.org/getLocations.php?f=json
-        runs: 1, // number of tests to run (1-10)
-        firstViewOnly: false, // if true runs first view only, will not run repeat view (will not test caching)
-        Private: true,
-        connections: 0, // override the number of concurrent connections IE uses (0 does not override)
-        web10: false, // set to true to force the test to stop at Document Complete (onLoad)
-        format: 'json', // do not change this
-        video: false, // set to true to capture video
-        medianVideoOnly: false, // set to true to only capture video for median run, see video above
-        notify: null, // email address to notify with test results summary
-        mobile: false, // set to true to have chrome emulate mobile browser
-        noOptimizations: true, // set to true to disable optimization checks (tests run faster when true)
-        noImages: false, // Set to true to disable screen shot capturing
-        noHeaders: false, // Set to true to disable saving of http headers, browser status messages and CPU utilization
-        noScript: false,
-    };
-
-    var defaults = {
-        tests: [],
-        mailto: null,
-        smtpServer: null,
-        emailSubject: null,
-        saveResultsTo: null,
-        saveRawDataTo: null,
-    };
+    var tests = [];
 
     grunt.registerTask('web-performance', function () {
-        var options = this.options(defaults);
-
+        var services = require('../lib/services/index')(grunt);
+        var options = this.options({ tests: [] });
         var taskComplete = this.async();
-        var tests = [];
 
-        function runTest(testOpts, next) {
-            var testOptions = _.extend({}, testDefaults, testOpts);
+        function runTest(testOptions, next) {
+            if (!testOptions.service) {
+                grunt.log.error('No test service provided for a test.');
+                next();
+                return;
+            }
 
-            var test = new TestModel(testOptions);
+            var service = services[testOptions.service.toLowerCase()];
+
+            if (!service) {
+                grunt.log.error('Unsupported service requested:', testOptions.service);
+                next();
+                return;
+            }
+
+            var test = new service(testOptions);
             tests.push(test);
 
-            grunt.verbose.writeln('Queuing test for:', testOptions.url);
+            grunt.log.writeln('Running test for:', test.toString());
 
-            test.queue(function (err) {
-                if (err) {
-                    grunt.log.error('Faile dto queue test for:', testOptions.url, err);
+            test.run(function (err) {
+                grunt.log.ok('Test finished, saving results.');
+
+                if (options.saveRawDataTo) {
+                    test.getExtraData(options.saveRawDataTo, next);
+                } else {
                     next();
-                    return;
                 }
+            });}
+        }
 
-                grunt.log.ok('Queued test for:', testOptions.url)
-                grunt.verbose.writeln('Waiting for results. Test id:', test.id);
+        grunt.util.async.forEachSeries(options.tests, runTest);
+    });
 
-                test.onReady(function () {
-                    grunt.log.ok('Test finished, saving results.');
+    // send out an email summary with highlights of recent results
+    grunt.registerTask('web-performance-email-summary', function () {
+        var nodemailer = require('nodemailer');
+        var jade = require('jade');
 
-                    // save data files
-                    if (options.saveResultsTo) {
-                        try {
-                            var saveAs = path.join(options.saveResultsTo, test.name + '-' + test.date + '.json');
-                            fs.writeFileSync(saveAs, test.testResultRawData, { encoding: 'utf8' });
-                            grunt.verbose.writeln('Saved raw test results to:', saveAs);
-                        } catch (ex) {
-                            grunt.log.error('Failed to save raw test results file.', ex);
-                        }
-                    }
+        var options = this.options({
+            nodemailer: {},
+            mailOptions: {},
+            sendAttachments: false
+        });
 
-                    console.log('Completed Test');
+        // create reusable transporter object using SMTP transport
+        var transporter = nodemailer.createTransport(options.nodemailer);
 
-                    // test.firstView.save(testOptions.label, true);
-                    // test.repeatView.save(testOptions.label, false);
-                    // console.log("Saved data to database");
+        var mailOptions = options.mailOptions;
 
-                    if (options.saveRawDataTo) {
-                        test.getExtraData(options.saveRawDataTo, next);
-                    } else {
-                        next();
-                    }
+        if (!mailOptions.subject) {
+            mailOptions.subject = 'grunt-web-performance summary report';
+        }
+
+        var results = '';
+        var attachments = [];
+
+        var getTestData = function (test, next) {
+            if (options.sendAttachments) {
+                attachments.push({
+                    filename: test.getFileName(),
+                    content: test.rawResults
                 });
-            });
-        }
-
-        function onTestsComplete() {
-            var async = false;
-
-            var helper = {
-                async: function () {
-                    async = true;
-
-                    return taskComplete;
-                }
-            };
-
-            options.onComplete.call(helper, tests);
-
-            if (!async) {
-                taskComplete();
             }
-        }
+        };
 
-        grunt.util.async.forEachSeries(options.tests, runTest, onTestsComplete);
+        var sendReport = function () {
+            mailOptions.html = jade.renderFile('email-summary.jade', {
+                tests: tests
+            });
+
+            if (options.sendAttachments) {
+                mailOptions.attachments = attachments;
+            }
+
+            transporter.sendMail(mailOptions, function (err, info) {
+                if (err) {
+                    console.warn("Failed to send email");
+                } else {
+                    console.log("Sent email:", info.response);
+                }
+            });
+        };
+
+        grunt.util.async.forEachSeries(tests, saveTest, sendReport);
+    });
+
+    grunt.registerTask('web-performance-save-results', function () {
+        var options = this.options({
+            directory: null
+        });
+
+        var saveTest = function (test, next) {
+            // save the results files
+            if (_.isFunction(test.getSaveFiles)) {
+                var files = test.getSaveFiles();
+
+                files.forEach(function (fileName, contents) {
+                    var saveAs = path.join(options.directory, fileName);
+                    fs.writeFileSync(saveAs, contents, { encoding: 'utf8' });
+                });
+            }
+
+            next();
+        };
+
+        grunt.util.async.forEachSeries(tests, saveTest);
     });
 };
